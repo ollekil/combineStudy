1a6f6c43f9e67eb5e17daf2ae538bff696cac0da
ollekil <ollekil@kakao.com>
2025-02-08 08:38:30 +0900

    Initial Commit

---
+++ CombineStudy/CombineStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineStudyApp.swift
+//  CombineStudy
+//
+//  Created by 배정길 on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
